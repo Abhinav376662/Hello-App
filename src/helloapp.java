714f00e8f9da3d057a5744d99c41572796d78a42
Abhinav376662 <abhinavasole2007@gmail.com>
2026-03-17 13:24:37 +0530

    C2 COMPLETED

--- src/helloapp.java
+++ src/helloapp.java
@@ -1,16 +1,14 @@
-
-// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 public class helloapp {
-   public helloapp() {
-   }
 
-   public static void main(String[] var0) {
-      if (var0.length > 0) {
-         String var1 = var0[0];
-         System.out.println("Hello, " + var1 + "!");
-      } else {
-         System.out.println("Hello, World!");
-      }
+    public static void main(String[] args) {
 
-   }
-}
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
+
+} 
